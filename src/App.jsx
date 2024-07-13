e23aca3d42f01cd97b383c7400b4c15a6943b0e4
Wilfred Muchire <logincoin40@gmail.com>
2024-07-13 12:15:02 +0300

    adding the header section

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,25 @@
-import { useState } from "react";
 import React from "react";
 import Blog from "./pages/Blogs/Blog";
 import "./App.css";
-
+import Header from "./components/Header";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Works from './pages/Works/Works'
+import Home from "./pages/Home/Home";
 function App() {
 
-  return <><Blog/></>;
+  return (
+    <>
+      <BrowserRouter>
+        <Header />
+        <Routes>
+          <Route exact path="/" element={<Home/>}/>
+          <Route exact path="/blogs" element={<Blog />} />
+          <Route exact path="works" element={<Works />} />
+        </Routes>
+      </BrowserRouter>
+
+    </>
+  );
 }
 
 export default App;
